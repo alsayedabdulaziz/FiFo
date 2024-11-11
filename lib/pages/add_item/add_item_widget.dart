576a4673371951d3eb091475e2d1c162434d9b0f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-11 14:09:46 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/add_item/add_item_widget.dart
+++ lib/pages/add_item/add_item_widget.dart
@@ -29,7 +29,7 @@ class _AddItemWidgetState extends State<AddItemWidget> {
 
     // On page load action.
     SchedulerBinding.instance.addPostFrameCallback((_) async {
-      FFAppState().scannerdata = ' ';
+      FFAppState().scannerdata = '';
       safeSetState(() {});
       _model.instantTimer = InstantTimer.periodic(
         duration: const Duration(milliseconds: 1000),
